4a969e957fe33e2d99dfa27ed42905d1c082e65e
Leo <leo.maedje@gmail.com>
2022-07-01 14:51:04 +0100

    Wrote basic framework for numAndStr

--- 1e-number-and-string.js
+++ 1e-number-and-string.js
@@ -6,6 +6,14 @@ asString - a string representation of the number
 Example: [4, -3.2] -> [{ asNumber: 4, asString: '4' }, { asNumber: -3.2, asString: '-3.2' }]
 */
 // function: numAndStr
-// parameters: inputNum, number
+// parameters: inputNums, array of numbers
 // returns: array of plain objects
 
+function numAndStr(inputNums) {
+    return numStrArray;
+}
+
+console.log(`numAndStr([]) returns: `, numAndStr([]), `should return: []`);
+console.log(`numAndStr([1]) returns: `, numAndStr([1]), `should return: [{ asNumber: 1, asString: '1' }]`);
+console.log(`numAndStr([-1, 0, 1]) returns: `, numAndStr([-1, 0, 1]), `should return: [{ asNumber: -1, asString: '-1' }, { asNumber: 0, asString: '0' }, { asNumber: 1, asString: 1 }]`);
+console.log(`numAndStr([-1.1, 0, 1.1]) returns: `, numAndStr([-1.1, 0, 1.1]), `should return: [{ asNumber: -1.1, asString: '-1.1' }, { asNumber: 0, asString: '0' }, { asNumber: 1.1, asString: 1.1 }]`);
