b7aae46bb84395205ad03c4887fb791810730ff4
Leo <leo.maedje@gmail.com>
2022-07-01 14:57:23 +0100

    Created 1f-pokemon-types.js and copied in task description

---
+++ 1f-pokemon-types.js
@@ -0,0 +1,7 @@
+/*
+1f: Pokemon types
+Challenge: Write a function that takes an array of objects representing Pokémon, with string properties name and type, and returns a descriptive sentence about them and their type: "<name> is a <type in lowercase> type Pokémon.".
+Example:
+[{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]
+-> ["Bulbasaur is a grass type Pokémon.", "Charmander is a fire type Pokémon.", "Squirtle is a water type Pokémon."] 
+*/
