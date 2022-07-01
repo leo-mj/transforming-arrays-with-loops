bbd8479028fa56b0e31c370577946b4f6ce60816
Leo <leo.maedje@gmail.com>
2022-07-01 14:46:25 +0100

    Created 1e and added task description

---
+++ 1e-number-and-string.js
@@ -0,0 +1,11 @@
+/*
+1e: Number and string
+Challenge: Write a function that takes an array of numbers and returns an array of plain objects, with properties:
+asNumber - the original number
+asString - a string representation of the number
+Example: [4, -3.2] -> [{ asNumber: 4, asString: '4' }, { asNumber: -3.2, asString: '-3.2' }]
+*/
+// function: numAndStr
+// parameters: inputNum, number
+// returns: array of plain objects
+
