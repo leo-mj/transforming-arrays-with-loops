70b4a7dedfb9946226557f359919e0ef11b41664
Leo <leo.maedje@gmail.com>
2022-07-01 14:37:49 +0100

    Wrote basic framework for onlyInitials

---
+++ 1d-only-initials.js
@@ -0,0 +1,18 @@
+/*
+1d: Only initials
+Challenge: Write a function that takes an array of names (always <firstname> <surname> format) and returns an array of initials for each person.
+Example: ["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"] -> ["A.K.", "L.H.", "Y.M."]
+*/
+// function: onlyInitials
+// parameters: nameArr, array of strings
+// returns: array of strings
+/**
+ * Takes an array of names (always <firstname> <surname> format) and returns an array of initials for each person.
+ * @param {string[]} nameArr - array of names
+ * @returns {string[]} - array of initials for each name in nameArr
+ */
+
+function onlyInitials(nameArr) {
+    return initialArr;
+}
+
