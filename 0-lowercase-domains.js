06fdc105eb02d880963da79ee60fd3afb9a42961
Leo <leo.maedje@gmail.com>
2022-07-01 13:59:37 +0100

    lowerCaseArray code runs

---
+++ 0-lowercase-domains.js
@@ -0,0 +1,21 @@
+/**
+ * Takes an array of strings representing domain names, in mixed case, and returns an array of the domain names in all-lowercase.
+ * @param {string[]} inputArray - array of strings in mixed case
+ * @returns {string[]} - array of strings in lower case
+ */
+
+
+
+function lowerCasesArray(inputArray) {
+    let outputArray = [];
+    for (let string of inputArray) {
+        let lowerCaseStr = string.toLowerCase();
+        outputArray.push(lowerCaseStr);
+    }
+    return outputArray;
+}
+
+// test cases:
+console.log('lowerCasesArray(["Hello", "how", "are", "you?"]) returns:', lowerCasesArray(["Hello", "how", "are", "you?"]), '; should return: ["hello", "how", "are", "you?"');
+console.log('lowerCasesArray([]) returns:', lowerCasesArray([]), '; should return: []');
+console.log('lowerCasesArray(["hi"]) returns:', lowerCasesArray(["hi"]), '; should return: ["hi"] ');
